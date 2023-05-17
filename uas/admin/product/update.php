215f32323333987b2294be189352c2f0df8ca635
Kenji <kyascentius11@students.calvin.ac.id>
2023-05-17 23:37:44 +0700

    Added php files

---
+++ uas/admin/product/update.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($_POST["btnSubmit"])) {
+	$id_input = $_POST["id"];
+	$produk = $_POST["produk"];
+	$harga = $_POST["harga"];
+    $image = $_POST["image"];
+
+include "../connection.php";
+
+
+$query = "UPDATE `products` SET products_name='$produk', price='$harga', `image`='$image' WHERE products_id='$id_input'";
+mysqli_query($conn, $query);
+$num = mysqli_affected_rows($conn);
+
+if ($num > 0) {
+   echo "Data yang kamu simpan sudah disimpan."; 
+?>
+   <meta content="0; url=product_manage.php?pesan=update" http-equiv="refresh">
+<?php
+} else {
+   echo "Data gagal disimpan ke dalam database.";
+}
+}
+?>  
