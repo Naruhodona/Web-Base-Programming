8806a0937b0e9e5e2c702f50cd1901512b38f928
Naruhodona <fredrikfernando2018@gmail.com>
2023-05-20 21:55:10 +0700

    ubah tempathistory transaksi ke page admin

--- uas/admin/admin.php
+++ uas/admin/admin.php
@@ -21,5 +21,46 @@
 	<a href="history/report_transaction.php">Report Transaction</a>
 	<a href="product/product_manage.php">Manage Products</a>
 	<a href="adminlogout.php">Logout</a>
+	<div class="content">
+        <div>
+            All Transaction
+            <?php
+            $sql = "SELECT transaction.transaction_date, account.username, transaction.total_price  FROM transaction INNER JOIN account ON transaction.user_id = account.user_id";
+            $result = mysqli_query($conn, $sql);
+            ?>
+            <table border="1">
+                <tr>
+                    <th>Transaction Date</th>
+                    <th>Username</th>
+                    <th>Spend</th>
+                </tr>
+                <?php
+                $sum = 0;
+                if (mysqli_num_rows($result) > 0) {
+                    $no = 1;
+                    
+                    while ($row = mysqli_fetch_assoc($result)) {                       
+                        
+                ?>
+                <tr>
+                    <td><?php echo $row['transaction_date']; ?></td>
+                    <td><?php echo $row['username']; ?></td>
+                    <td><?php echo $row['total_price']; ?></td>
+                </tr>
+                <?php
+                    $sum += $row['total_price'];
+                    }
+                }else {
+                    echo "<tr><td colspan='3'>Tidak ada data.</td></tr>";
+                }
+                ?>
+                <tr>
+                    <td colspan="2">Total</td>
+                    <td><b><?php echo $sum; ?></b></td>
+                </tr>
+            </table>
+        </div>
+        <a href="../admin.php">Back</a>
+    </div>
 </body>
 </html>
